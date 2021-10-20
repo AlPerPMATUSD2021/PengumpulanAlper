5c6c30d75908ee9f5cd28831f2fce00689718797
magdalenasdw <magdalenasdw@gmail.com>
2021-10-20 14:53:38 +0800

    Tugas Kelompok 3

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function keliling (a,b,c) {
+    return k = a + b + c 
+}
+
+console.log(keliling(4,2,3));
+
+function luas (s) {
+    return l = 6 * s * s
+}
+
+console.log(luas(4));
+
+function keliling(s) {
+    return m = 4 * s
+}
+
+console.log(keliling(5));
