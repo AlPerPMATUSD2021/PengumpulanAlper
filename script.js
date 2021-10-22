54a7083d1236ad9e2415b0e23ba58e27851dfd76
unknown <aurel210723@gmail.com>
2021-10-22 19:02:28 +0700

    pengumpulan alper kelompok 6

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-function rumusjarak() {
-    let j, k, w
-    k=Number(document.formscript.kecepatan.value);
-    w=Number(document.formscript.waktu.value);
-    j = k * w;
-    document.formscript.jarak.value=j;
-}
-
-function volumelimas(){
-    let v, s, t;
-    s=Number(document.formscript.luasalas.value);
-    t=Number(document.formscript.tinggi.value);
-    v = s * t
+function volumekubus() {
+    let v, s
+    s=Number(document.formscript.sisi.value);
+    v = s * s * s;
     document.formscript.volume.value=v;
 }
 
-function Llayanglayang() {
-    let L, a, b;
-    a=Number(document.formscript.diagonal1.value);
-    b=Number(document.formscript.diagonal2.value);
-    L = (1/2) * a * b;
-    document.formscript.luas.value=L;
+function kelilingpersegipanjang() {
+    let K, p, l
+    p=Number(document.formscript.panjang.value);
+    l=Number(document.formscript.lebar.value);
+    K = 2 * (p+l);
+    document.formscript.keliling.value=K;
+}
+function persamaanlingkaran() {
+    let a, x, b, y, r
+    a=Number(document.formscript.titikkoordinat1.value);
+    b=Number(document.formscript.titikkoordinat2.value);
+    x=Number(document.formscript.variable1.value);
+    y=Number(document.formscript.variable2.value);
+    r = Math.sqrt(Math.pow((x-a),2) + Math.pow((y-b),2));
+    document.formscript.jarijari.value=r;
 }
